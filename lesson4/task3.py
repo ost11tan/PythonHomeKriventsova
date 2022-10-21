616ebc53a2e5a09ef089b0c45c25dad78fd77337
tati <ost11tan@gmail.com>
2022-10-21 08:53:33 +0300

    Задание 3

---
+++ lesson4/task3.py
@@ -0,0 +1,22 @@
+#3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+n = int(input("Введите количество элементов списка "))
+summ=0
+if n<1:
+    print ("Данное значение не удовлетворяет условию задачи,введите число больше или равное 1")
+else:
+    num = []
+    for i in range(n) : 
+        num.append(random.randint(0, 10)) 
+    print(num)
+    
+    
+    #list2=list(set(num))   решение задачи через функцию set
+    #print (list2)
+    
+    num_uniq = []
+    for i in range(n) : 
+        if num[i] not in num_uniq:
+            num_uniq.append(num[i])
+    print(num_uniq)
