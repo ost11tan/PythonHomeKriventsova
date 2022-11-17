dc9f72093e889f48d0b79042de52982955a3b50e
tati <ost11tan@gmail.com>
2022-11-17 09:51:28 +0300

    Остался главный и логи

---
+++ lesson7/changes_stroki.py
@@ -0,0 +1,30 @@
+import export_stroki as ex
+import list_to_stroka as to_stroka
+     
+
+def ch_strok():
+    lst=ex.ex_strok()
+    print(lst)
+
+    num_elem=int(input("Введите цифру,соответствующую столбцу\n0-Имя\n1-Фамилия\n2-Телефон\n3-Комментарий  \n"))
+
+    elem=lst[num_elem]
+    #print(ex.num_strok)
+    
+    elem=input("Введите значение, на которoе вы хотите заменить "+ str(elem)+"  " )
+    lst[num_elem]=elem
+    
+    new_str=to_stroka.lst_to_str(lst)
+    old_str=ex.table[ex.num_strok]
+    
+    
+    
+    with open("telefone.csv") as f:
+        s=f.read()
+    with open("telefone.csv", 'w') as f:
+        s=s.replace(old_str,new_str)
+        f.write(s)
+    
+
+
+ch_strok()
